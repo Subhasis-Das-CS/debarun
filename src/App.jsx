81f95b2889ade896e70adb23b08f6b978c377424
Subhasis <subhashishdas2000@gmail.com>
2025-12-28 21:56:25 +0530

    Updated on 28th DEC, fetching added

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Data from './modules/Data';
 import style from './modules/stylesheet.module.css';
 import app from './firebase'; 
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
-import { getFirestore, addDoc, collection } from 'firebase/firestore';
+import { getFirestore, addDoc, collection, CollectionReference, getDocs } from 'firebase/firestore';
 function App() {
 
   const [a, setA]= useState(0);
@@ -14,35 +14,36 @@ function App() {
   const auth= getAuth(app);
   const db= getFirestore(app);
 
+
   return (
     <>
       <div>
 
-        <p className={style.abc}>HI</p> {a}
+        {/* <p className={style.abc}>HI</p> {a} */}
    
-   <Data name="Debarun" address="school para"/>
+   {/* <Data name="Debarun" address="school para"/> */}
+{/* 
+        <button onClick={()=>{setA(a+1)}}>Click </button> */}
 
-        <button onClick={()=>{setA(a+1)}}>Click </button>
-
-        <input type='text' id="email"></input>
-        <input type='text' id="password"></input>
-        <input type="text" id="address"></input>
-        <input type="text" id="XYZ"></input>
+        <input type='text' placeholder='Enter Product name' id="name"></input> <br/>
+        <input type='text' id="price" placeholder='Enter price'></input> <br/>
+        <input type='text' id="quantity" placeholder='Enter quantity'></input><br/>
+        
         <button onClick={()=>{
-            const mail=document.getElementById('email').value;
-             const pass=document.getElementById('password').value;
-             const addr=document.getElementById('address').value;
-             const random=document.getElementById('XYZ').value;
+            const pname=document.getElementById('name').value;
+             const pprice=document.getElementById('price').value;
+             const pquantity=document.getElementById('quantity').value;
+             
 
              try{
-                createUserWithEmailAndPassword(auth, mail, pass);
+                // createUserWithEmailAndPassword(auth, pname, pprice);
 
 
                 const document={
-                  email: mail,
-                  password: pass,
-                  address:addr,
-                  XYZ:random
+                  name: pname,
+                  price: pprice,
+                  quantity: pquantity
+                  
                 }
 
                 
@@ -53,6 +54,22 @@ function App() {
                 console.log(e);
              }
         }}  > Sign up</button>
+
+        <button onClick={async ()=>{
+             
+              const col=collection(db, "users");
+             const a= await getDocs(col);
+             a.forEach((doc)=>{
+              if(doc.data().price<=10000)
+              console.log(doc.data().name);
+             })
+
+
+          
+
+
+
+        }}  >Fetch</button>
        </div>
     </>
   )
